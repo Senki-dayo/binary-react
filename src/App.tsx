10e07ab4ad51a260feef75db4974cdda94d7c5f6
岩永千輝 <52753117+Senki-dayo@users.noreply.github.com>
2023-12-22 23:17:57 +0000

    Update 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import "./styles.css";
 
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Tabs>
+      <TabList>
+        <Tab>HOME</Tab>
+        <Tab>About</Tab>
+        <Tab>Contact</Tab>
+      </TabList>
+
+      <TabPanel>
+        <h1>HOMEです</h1>
+      </TabPanel>
+      <TabPanel>
+        <h1>Aboutです</h1>
+      </TabPanel>
+      <TabPanel>
+        <h1>Contactです</h1>
+      </TabPanel>
+    </Tabs>
   );
 }
